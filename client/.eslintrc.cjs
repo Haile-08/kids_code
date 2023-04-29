e3cf6584bc8ced4c7f21ff8066863c710c21bab1
Haile-08 <haile4cmd@gmail.com>
2023-04-29 09:49:52 +0300

    Setup airbnb eslint config

--- client/.eslintrc.cjs
+++ client/.eslintrc.cjs
@@ -1,15 +1,38 @@
 module.exports = {
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
-  ],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		"airbnb", 
+		"airbnb/hooks",
+		'plugin:react/recommended'
+	],
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
---
+++ client/src/test/server/handlers/auth.jsx
@@ -0,0 +1,17 @@
+import { rest } from "msw"
+import { API_URL } from "../../config";
+
+export const authHandlers = [
+    rest.post(`${API_URL}/auth/login`, (req, res, ctx)=>{
+        try {
+            const result = {name:"haile", password: "password"};
+            return res(
+                ctx.status(200),
+                ctx.json(result)
+            )
+        } catch (error) {
+            ctx.status(400)
+            ctx.json({message: error?.message || "server error"})
+        }
+    })
+]
