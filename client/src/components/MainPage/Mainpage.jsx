49ccfa36fa5ed99e508df5256e23fb6924240a03
Haile-08 <haile4cmd@gmail.com>
2023-05-04 08:40:15 +0300

    Add logout rout and dispatch

--- client/src/components/MainPage/Mainpage.jsx
+++ client/src/components/MainPage/Mainpage.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 import image from '../../assets/a.png';
+import { setLogout } from '../../state/authSlice';
 
 function Mainpage() {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    dispatch(setLogout());
+    navigate('/');
+  };
   return (
     <div className="mainpage">
       <div className="profile">
         <div className="nav">
           <img src={image} alt="image" />
           <p>Haile</p>
-          <button>Logout</button>
+          <button onClick={() => handleLogout()}>Logout</button>
         </div>
       </div>
       <div className="mainpart">
