a536aa01a3a327aea2d21f24e1e9ae34b542b509
Haile-08 <haile4cmd@gmail.com>
2023-06-25 01:35:54 +0300

    Homepage

--- client/src/components/Elements/navbar/NavBar.jsx
+++ client/src/components/Elements/navbar/NavBar.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 import logo from '../../../assets/logo.png';
 import nav from '../../../assets/nav.png';
 import './navbar.css';
+import { useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function NavBar() {
+  const navigate = useNavigate();
+
+  const handleHomeNav = () => {
+    navigate('/');
+  };
   return (
     <div className="navbar">
       <div className="logo">
-        <img src={logo} alt="logo" />
+        <img src={logo} alt="logo" onClick={() => handleHomeNav()} />
+      </div>
+      <div className="nav1">
+        <NavLink to="/">
+          <p>Home</p>
+        </NavLink>
+        <p>About</p>
+      </div>
+      <div className="nav2">
+        <p>Login</p>
+        <p>Signup</p>
       </div>
       <div className="navlogo">
         <img src={nav} alt="nav" />
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import './Home.css';
-import introImage from '../../assets/Gaming.gif';
 import NavBar from '../Elements/navbar/NavBar';
+import bot from '../../assets/bot.svg';
+import redbot from '../../assets/redbot.png';
+import greenbot from '../../assets/greenbot.png';
+import yellowbot from '../../assets/yellowbot.png';
 
 function Home() {
   return (
     <div className="main">
       <NavBar />
       <div className="mainHome">
-        <div className="text">
-          <p>Code kid a Fun and Interactive Coding Experience for kids</p>
-        </div>
-        <div className="image">
-          <img src={introImage} alt="imageintro" />
-        </div>
+        <h1>Codie Kid</h1>
+        <img src={bot} alt="bot" />
+      </div>
+      <div className="foot">
+        <div className="line"></div>
+        <p>Game</p>
+        <div className="line"></div>
+      </div>
+      <div className="redbot">
+        <p>RedBot</p>
+        <img src={redbot} alt="redbot" />
+      </div>
+      <div className="greenbot">
+        <p>GreenBot</p>
+        <img src={greenbot} alt="greenbot" />
+      </div>
+      <div className="yellowbot">
+        <p>YellowBot</p>
+        <img src={yellowbot} alt="yellowbot" />
       </div>
     </div>
   );
